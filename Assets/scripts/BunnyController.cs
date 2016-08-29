051af4731ddeba11a86d04ff7730f253dd6be590
Alvin Tatenda Vafana <alvintvafana@gmail.com>
2016-08-29 03:52:01 +0200

    bunny animated

--- Assets/scripts/BunnyController.cs
+++ Assets/scripts/BunnyController.cs
@@ -6,8 +6,10 @@ public class BunnyController : MonoBehaviour {
 	// Use this for initialization
     private Rigidbody2D myRigidBody2D;
     public float bunnyJumpForce = 500f;
+    private Animator myAnime;
 	void Start () {
         myRigidBody2D = GetComponent<Rigidbody2D>();
+        myAnime = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -17,5 +19,6 @@ public class BunnyController : MonoBehaviour {
         myRigidBody2D.AddForce(transform.up * bunnyJumpForce);
         //System.IO.File.WriteAllText(@"c:\musvo\unity.txt", "test");
     }
+    myAnime.SetFloat("vVelocity", myRigidBody2D.velocity.y);
 	}
 }
